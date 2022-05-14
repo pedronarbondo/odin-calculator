505ddaa132d7c2f5f918f982e6cd1a670d134f1a
pedronarbondo <pedro.narbondo@hotmail.com>
2022-05-14 15:37:15 -0300

    Fix display bugs, start adding button logic

--- script.js
+++ script.js
@@ -10,7 +10,52 @@ let substractButton = document.getElementById("substract")
 let sumButton = document.getElementById("sums");
 let equalsButton = document.getElementById("equals")
 let decimalButton = document.getElementById("decimal"); 
-let digits = document.getElementsByClassName("numberData");
+let bottomDisplay = document.querySelector(".bottom-display");
+let topDisplay = document.querySelector(".top-display");
+
+/* I know this next method would have been easier if I had just put the same class
+to all digits and then querySelected by class, but I couldn't figure out how to
+then assign the value each digit had to have*/
+let zero = document.querySelector(".zero");
+let one = document.querySelector(".one");
+let two = document.querySelector(".two");
+let three = document.querySelector(".three");
+let four = document.querySelector(".four");
+let five = document.querySelector(".five");
+let six = document.querySelector(".six");
+let seven = document.querySelector(".seven");
+let eight = document.querySelector(".eight");
+let nine = document.querySelector(".nine");
+
+zero.addEventListener("click", () => {
+    if (bottomDisplay.textContent != `${/^00/}`){
+    bottomDisplay.textContent = `${bottomDisplay.textContent}${zero.textContent}`}})
+one.addEventListener("click", () => bottomDisplay.textContent = 
+`${bottomDisplay.textContent}${one.textContent}`)
+two.addEventListener("click", () => bottomDisplay.textContent = 
+`${bottomDisplay.textContent}${two.textContent}`)
+three.addEventListener("click", () => bottomDisplay.textContent = 
+`${bottomDisplay.textContent}${three.textContent}`)
+four.addEventListener("click", () => bottomDisplay.textContent = 
+`${bottomDisplay.textContent}${four.textContent}`)
+five.addEventListener("click", () => bottomDisplay.textContent = 
+`${bottomDisplay.textContent}${five.textContent}`)
+six.addEventListener("click", () => bottomDisplay.textContent = 
+`${bottomDisplay.textContent}${six.textContent}`)
+seven.addEventListener("click", () => bottomDisplay.textContent = 
+`${bottomDisplay.textContent}${seven.textContent}`)
+eight.addEventListener("click", () => bottomDisplay.textContent = 
+`${bottomDisplay.textContent}${eight.textContent}`)
+nine.addEventListener("click", () => bottomDisplay.textContent = 
+`${bottomDisplay.textContent}${nine.textContent}`)
+
+function clearScreens(){
+    topDisplay.textContent = "";
+    bottomDisplay.textContent = "";
+    return;
+}
+clearButton.addEventListener("click", clearScreens);
+
 
 function sum(a, b){
     return a + b
